ed819ee208b14f4c5d8995d83c4c2e2a93e3c9ed
lovequentin <lovequentin@naver.com>
2019-06-14 09:31:04 +0900

    my first web page
    this is my first web page

---
+++ weather.js
@@ -0,0 +1,53 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "0680b89cc08c6dfc4fb8d2f4afc97efa";
+const COORDS = "coords";
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric
+    `).then(function(response){
+        return response.json()
+    }).then(function(json){
+        console.log(json);
+        const temperature = json.list.main;
+        const place = json.city.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError(){
+    console.log('Cant access geo location');
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else{
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+init()
